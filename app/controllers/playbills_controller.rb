7e9c70f12a31a424601415146d45b26719e2f083
NickSpangler <Spangleyman@mac.com>
2021-01-13 15:05:10 -0500

    remove local path

--- app/controllers/playbills_controller.rb
+++ app/controllers/playbills_controller.rb
@@ -15,10 +15,11 @@ class PlaybillsController < ApplicationController
 
     post "/playbills" do
             tempfile = Down.download("#{params[:image_url]}")
-            image_path = "/Users/nickspangler/Flatiron/code/my-playbill-vault/public/images/downloaded_playbills/#{to_param(params[:image_title])}.jpeg"
-            # if !File.exist?("public/images/downloaded_playbills/#{to_param(params[:image_title])}.jpeg")
-            #     FileUtils.mv(tempfile.path, "#{image_path}")
-            # end
+            # image_path = "/Users/nickspangler/Flatiron/code/my-playbill-vault/public/images/downloaded_playbills/#{to_param(params[:image_title])}.jpeg"
+            image_path = "/public/images/downloaded_playbills/#{to_param(params[:image_title])}.jpeg"
+            if !File.exist?("public/images/downloaded_playbills/#{to_param(params[:image_title])}.jpeg")
+                FileUtils.mv(tempfile.path, "#{image_path}")
+            end
             params[:image_url] = "/images/downloaded_playbills/#{to_param(params[:image_title])}.jpeg"
             params.tap { |param| param.delete(:image_title) }
             @playbill = current_user.playbills.create(params)
